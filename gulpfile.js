4f416aa0345149be39126d1ebddebccf148cb097
Egor Tokarev <zippper123915@gmail.com>
2019-11-08 07:17:03 +0300

    Все коммиты слетели ужас

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
 
 function style () {
     return gulp.src('./media/styles/scss/*.scss')
